849b0746ecbc1cf450bd1971fbabd3f4517b53a1
Tomas Escobar <thomastix100@gmail.com>
2020-02-16 04:59:36 -0500

    Esta version de busqueda de posiciones esta listo a nivel de filas y columnas

--- Metodo_BusparPosOthello.py
+++ Metodo_BusparPosOthello.py
@@ -1,69 +1,56 @@
-state = [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0, 0, 0], [0, 0, 0, 1, 2, 0, 0, 0], [0, 0, 0, 2, 1, 0, 0, 0], [0, 0, 2, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]]
+"""ESte metodo te saca las jugadas que puedes hacer a nivel de filas y columnas del Othelo,
+La siguiente version sera sacar los movimientos de las diagonales"""
 
-lista = []
-
-
-player = 2
-tamano = len(state)
-
-#Para mirar si una tupla esta en una lista
-#(1,2) in lis
-
-"""for i in range(len(state)):
-    for j in range(len(state[i])):
-        if state[i][j] == player:
-            lis.append((i,j))"""
-
-contrincante = 0
-if player == 2:
-    contrincante = 1
-elif player == 1:
-    contrincante = 2
-
-
-
-for i in range(len(state)):
-    if state[i].count(player) != 0 and (tamano - state[i].count(0)) != 1:
-        estaDentroDeBlancas = 0
-        estaDentroDeNegras = 0
-        verdeEnNegra = 0
-        verdeEnBlancas = 0
-        posX = 0
-        posY = 0
-        lisT = []
-        for j in range(len(state[i])):
-            
-            if state[i][j] == contrincante:
-                if estaDentroDeNegras == 1:
-                    verdeEnNegra = 1
-                if estaDentroDeBlancas == 1:
-                    verdeEnBlancas = 1
-
-            if state[i][j] == 0:
-                posX = i
-                posY = j
-                estaDentroDeBlancas = 1
-                estaDentroDeNegras = 0
-
-                if verdeEnNegra == 1:
-                    lisT.append(i)
-                    lisT.append(j)
-                    lista.append(lisT)
-                    lisT = []
-                    verdeEnNegra = 0
-
-            if state[i][j] == player:
-                estaDentroDeNegras = 1
+def return_positions(self,contrincante, player, lista, state,parametro):
+        tamano = len(state)
+        for i in range(len(state)):
+            if state[i].count(player) != 0 and (tamano - state[i].count(0)) != 1:
                 estaDentroDeBlancas = 0
+                estaDentroDeNegras = 0
+                verdeEnNegra = 0
+                verdeEnBlancas = 0
+                posX = 0
+                posY = 0
+                lisT = []
+                for j in range(len(state[i])):
+                    if state[i][j] == contrincante:
+                        if estaDentroDeNegras == 1:
+                            verdeEnNegra = 1
+                        if estaDentroDeBlancas == 1:
+                            verdeEnBlancas = 1
 
-                if verdeEnBlancas == 1:
-                    lisT.append(posX)
-                    lisT.append(posY)
-                    lista.append(lisT)
-                    lisT = []
-                    verdeEnBlancas = 0
-                
+                    if state[i][j] == 0:
+                        posX = i
+                        posY = j
+                        estaDentroDeBlancas = 1
+                        estaDentroDeNegras = 0
 
+                        if verdeEnNegra == 1:
+                            if parametro == 1:
+                                lisT.append(j)
+                                lisT.append(i)
+                            else:
+                                lisT.append(i)
+                                lisT.append(j)
+                            if lisT not in lista:
+                                lista.append(lisT)
+                                lisT = []
+                            verdeEnNegra = 0
 
+                    if state[i][j] == player:
+                        estaDentroDeNegras = 1
+                        estaDentroDeBlancas = 0
 
-print(lista)
+                        if verdeEnBlancas == 1:
+                            if parametro == 1:
+                                lisT.append(posY)
+                                lisT.append(posX)
+                            else:
+                                lisT.append(posX)
+                                lisT.append(posY)
+
+                            if lisT not in lista:
+                                lista.append(lisT)
+                                lisT = []
+                            verdeEnBlancas = 0
+        return lista
